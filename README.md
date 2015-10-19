99c7ba90eb0afad20bf704d79ed82f1f98827fb1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-10-19 13:09:02 +0100

    feat: initial setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ng-webpack-play
+
+## Installation
+
+* `npm install`
+
+## Play
+
+* `npm run play` to start a web server and watch for changes
+* `npm run build` to prepare a distributable bundle
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,14 @@
 module.exports = {
-    entry: "./app",
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js"
-    },
-    resolve: {
-        extensions: ['', '.js', '.ts']
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.ts/,
-                loaders: ['ts-loader'],
-                exclude: /node_modules/
-            }
-        ]
-    },
-    devtool: ['eval', 'source-map']
+  entry: "./src/hello",
+  output: {
+    path: __dirname + "/dist", publicPath: 'dist/', filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+  module: {
+    loaders: [{
+      test: /\.ts/, loaders: ['ts-loader'], exclude: /node_modules/
+    }]
+  }
 };
