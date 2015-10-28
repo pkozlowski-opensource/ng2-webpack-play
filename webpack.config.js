858a9f9bf0a788622468b41a47bccf9e12cd0a1b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-10-28 13:45:13 +0100

    feat: use ng2 bundles with external Rx

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ module.exports = {
   output: {
     path: __dirname + "/dist", publicPath: 'dist/', filename: "bundle.js"
   },
+  externals: [{'angular2/angular2': 'var ng'}],
   resolve: {
     extensions: ['', '.js', '.ts']
   },
